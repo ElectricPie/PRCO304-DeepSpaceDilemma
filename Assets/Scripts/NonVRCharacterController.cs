47def02617fb777ff3f8da4dd145b8f8c1f3cfae
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-09 14:21:33 +0000

    Created Basic NonVRController
    Created a character that can move using the 'WASD' keys.

---
+++ Assets/Scripts/NonVRCharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonVRCharacterController : MonoBehaviour
+{
+    //Public
+    public float speed = 1.0f;
+    public float gravity = 9.8f;
+
+    //Private
+    private CharacterController m_characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_characterController = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get movement input
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        //Add gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        //Apply movement to the character
+        m_characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
