97d5f6c570e53cbf79a4ac5d6b5cbbce7c0c2c36
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-10 23:20:41 +0000

    Created Basic VR Scene
    Created a basic scene that has a XR rig allowing VR to be used.
    A cube is placed in the world which the player can grab.

---
+++ Assets/Scripts/VRCharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRCharacterController : MonoBehaviour
+{
+    //Private
+    private Camera m_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_camera = this.GetComponent<Camera>();
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
