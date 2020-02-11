23bd009c39578e6e7657c8ad904f52a8f1b8ca32
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-11 21:33:14 +0000

    Implemented VR Movement
    Updated input axis so that they correctly read the joysticks.
    Added character controller to the VR Character and a VR Controller script which moves the character based on left controllers input.

--- Assets/Scripts/VRCharacterController.cs
+++ Assets/Scripts/VRCharacterController.cs
@@ -5,17 +5,43 @@ using UnityEngine.XR;
 
 public class VRCharacterController : MonoBehaviour
 {
+    //Public
+    public float speed = 5.0f;
+    public float gravity = 9.8f;
+
+    //Private
+    private CharacterController m_characterController;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        m_characterController = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
         Debug.Log("Left: " + Input.GetAxis("VRSecondaryAxisLeftX") + " | " + Input.GetAxis("VRSecondaryAxisLeftY"));
-        Debug.Log("Right: " + Input.GetAxis("VRSecondaryAxisRightX") + " | " + Input.GetAxis("VRSecondaryAxisRightY"));
+
+        //Get movement input
+        Vector3 moveDirection = Vector3.zero;
+        // Gets the forward vector so that forward/backward movement will happen
+        moveDirection += this.transform.TransformDirection(Vector3.forward) * Input.GetAxis("VRSecondaryAxisLeftY");
+        //Gets the right vector so that right/left movement will happen
+        moveDirection += this.transform.TransformDirection(Vector3.right) * Input.GetAxis("VRSecondaryAxisLeftX");
+        moveDirection *= speed;
+
+        //Add gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        //Apply movement to the character
+        m_characterController.Move(moveDirection * Time.deltaTime);
     }
 }
 
