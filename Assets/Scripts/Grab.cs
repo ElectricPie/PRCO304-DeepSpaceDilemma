169cbc3b20683978aa12d15d7f9c0d92e72c7781
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-14 15:52:13 +0000

    Player can Grab Objects
    Created a script which attaches an object to the players hand if they press the grab key.
    The player can only grab objects with the grabable object script.

--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -11,6 +11,7 @@ public class Grab : MonoBehaviour
     //Private 
     private string m_handGrip;
     private GameObject m_lastCollision;
+    private GameObject m_grabbedObject;
 
     // Start is called before the first frame update
     void Start()
@@ -28,20 +29,55 @@ public class Grab : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis(m_handGrip) > 0.5)
+        //Gets the grab input
+        if (Input.GetAxis(m_handGrip) > 0.5)
         {
-            Debug.Log("Grabed");
+            //Check if an object is already be held
+            if (m_grabbedObject == null)
+            {
+                if (m_lastCollision != null)
+                {
+                    m_grabbedObject = m_lastCollision;
+                    m_lastCollision = null;
+
+                    //Set the collider to a trigger so that other things can still interact
+                    m_grabbedObject.GetComponent<Collider>().isTrigger = true;
+                    m_grabbedObject.GetComponent<Rigidbody>().useGravity = false;
+                    //Attach the object to the hand
+                    m_grabbedObject.transform.SetParent(this.transform);
+                }
+            }
+            else
+            {
+                //TODO: Drop item
+            }
         }
+        /*
+        if (m_grabbedObject != null)
+        {
+            if (m_grabbedObject.transform.parent != this)
+            {
+                Debug.LogError("Not parent");
+                m_grabbedObject = null;
+            }
+        }
+        */
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        m_lastCollision = other.gameObject;
+        //Debug.Log("Other Tag: " + other.tag);
+
+        if (other.GetComponent<GrabableObject>())
+        {
+            //Debug.Log("other: " + other);
+            m_lastCollision = other.gameObject;
+        }
     }
 
     private void OnTriggerEnteExit(Collider other)
     {
-        if (other.gameObject == m_lastCollision)
+        if (other.GetComponent<GrabableObject>() && other.gameObject == m_lastCollision)
         {
             m_lastCollision = null;
         }
