58eda94d4867d56587d57df0c7566a2bb2eacf5e
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-14 15:06:22 +0000

    Started Custom Grab Script
    Added a new script that will handle grabbing.
    Currently the script will send a debug message when either grip is pressed and will store the last object pass through the trigger.

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Grab : MonoBehaviour
+{    
+    //Public
+    public bool isLeftHand;
+
+    //Private 
+    private string m_handGrip;
+    private GameObject m_lastCollision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLeftHand)
+        {
+            m_handGrip = "VRSecondaryAxisLeftGrab";
+        }
+        else
+        {
+            m_handGrip = "VRSecondaryAxisRightGrab";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis(m_handGrip) > 0.5)
+        {
+            Debug.Log("Grabed");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_lastCollision = other.gameObject;
+    }
+
+    private void OnTriggerEnteExit(Collider other)
+    {
+        if (other.gameObject == m_lastCollision)
+        {
+            m_lastCollision = null;
+        }
+    }
+}
