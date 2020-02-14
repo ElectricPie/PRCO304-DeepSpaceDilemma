7b272e0abc5575857b2fc53f9883085472c1d194
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-14 14:33:04 +0000

    Created Weapon Grab Script
    Added a script that disables a game objects collider when it is picked up as it caused an issue with collision which would propel the player character backwards.

---
+++ Assets/Scripts/Weapons/WeaponGrab.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class WeaponGrab : MonoBehaviour
+{
+    public Collider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Check if the public collider was set
+        if (collider == null)
+        {
+            //Get the collider on the object
+            this.GetComponent<Collider>();
+
+            //Checks if the gathered collider was added.
+            if (collider == null)
+            {
+                Debug.LogError("No collider found on: " + this);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }   
+    
+    public void GrabWeapon()
+    {
+        Debug.Log("Grabed Weapon");
+        collider.enabled = false;
+    }
+
+    public void DropWeapon()
+    {
+        Debug.Log("Dropped Weapon");
+        collider.enabled = true;
+    }
+}
