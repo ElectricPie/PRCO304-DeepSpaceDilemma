7998278158242e269b04f2a9fa8d6a1ff38143af
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-17 22:38:22 +0000

    Implimented Basic Weapon Firing
    When the player pulls the trigger whilst holding a weapon, a pick sphere will appear at the first object in a line from the front of the gun.

--- Assets/Scripts/Weapons/WeaponGrab.cs
+++ Assets/Scripts/Weapons/WeaponGrab.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class WeaponGrab : GrabableObject
 {
+    //Public
     public Vector3 grabPoint;
     public float grabRotation = 36.0f;
+
+    public int ammo = 30;
+    //TODO: Replace with sprite
+    public GameObject inpactDecal;
+
 
     // Start is called before the first frame update
     void Start()
@@ -45,6 +51,17 @@ public class WeaponGrab : GrabableObject
 
     public override void Interact()
     {
-        Debug.Log("Firering Weapon");
+        Debug.Log("Firing");
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            //TODO: Impliment damaging hit target
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Hit: " + hit.point);
+            //TODO: Replace with creating decal
+            GameObject tempImpact = Instantiate(inpactDecal);
+            tempImpact.transform.position = hit.point;
+        }
     }
 }
