fa0e66f47ae24e7a0122646ea5b88f078fd7cdd7
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-18 00:04:31 +0000

    Correctly Sets the Magazines Position
    Moving the magazine into the magZone trigger area will correctly move the magazine into the appropriate place.
    Also created a prefab of the test weapon.

--- Assets/Scripts/Weapons/MagZone.cs
+++ Assets/Scripts/Weapons/MagZone.cs
@@ -23,10 +23,19 @@ public class MagZone : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        //TODO: Prevent multiple magazines
         //Checks of the object has the magazine grab script
         if (other.GetComponent<MagazineGrab>())
         {
             m_weapon.Reload(other.gameObject);
+            //Attach the magazine to the weapon
+            other.transform.parent = m_weapon.gameObject.transform;
+            //Correctly sets the magazines position and rotation
+            other.transform.localPosition = this.transform.localPosition;
+            other.transform.localRotation = Quaternion.Euler(Vector3.zero);
+            //Prevents the new magazine from moving
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.GetComponent<Collider>().isTrigger = true;
         }
     }
 }
