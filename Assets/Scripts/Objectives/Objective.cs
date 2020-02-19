b8e48ea0241d44aea6cb304acb91088697c0f8e1
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-19 19:32:18 +0000

    Created a Objective Script
    Created a new script that will be the base for any future objectives. Currently it draws a gizmo the size of the collider so that the collider is easier to see during development.

---
+++ Assets/Scripts/Objectives/Objective.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnDrawGizmos()
+    {
+        //Create a transparent green cube the size of the collider
+        Gizmos.color = new Color(0.0f, 1.0f, 0.0f, 0.5f);
+        Gizmos.DrawCube(this.GetComponent<Collider>().bounds.center, this.GetComponent<Collider>().bounds.extents * 2);
+    }
+}
