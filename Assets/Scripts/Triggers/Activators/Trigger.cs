3a98057950a96ecd2ce754d86f443a18f60969b4
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-21 00:59:33 +0000

    Implimented Move Trigger Action
    The move trigger script now draws a gizmo to show its destination, When triggered the object with the script will move to the provided position.
    Added a array of trigger targets which can be set in the inspector and when the trigger is activated the script attempts to call all targets activation function.

---
+++ Assets/Scripts/Triggers/Activators/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject[] m_triggerTargets;
+
+    protected void ActivateTrigger()
+    {
+        for (int i = 0; i < m_triggerTargets.Length; i++)
+        {
+            //Checks if the target has a the required script and if so calls the activate
+            if (m_triggerTargets[i].GetComponent<ITriggerTarget>() != null) {
+                m_triggerTargets[i].GetComponent<ITriggerTarget>().Activate(); 
+            }
+            else
+            {
+                Debug.LogWarning(this + ": target \"" + i + "\" \"" + m_triggerTargets[i] + "\" is missing Trigger Target");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Triggers/Targets/TirggerTargetMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TirggerTargetMove : MonoBehaviour, ITriggerTarget
+{
+    //Public
+    public Vector3 targetDestination;
+
+    //Private
+    private Vector3 m_originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_originalPosition = this.transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        this.transform.position = targetDestination;
+    }
+
+    void OnDrawGizmos()
+    {
+        //Create a sphere where the object will move to
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(targetDestination, 0.05f);
+    }
+
+}
