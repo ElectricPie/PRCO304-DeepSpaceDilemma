f7300fa845c57693d1bc953a89f065961496857f
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-24 13:46:31 +0000

    Created Lever Grab Script
    Create a script which allows a player to grab a lever, when the player grabs the lever the object will rotate the x axis to match the players hand.

---
+++ Assets/Scripts/LeverGrab.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverGrab : GrabableObject
+{
+    public float adjustmentAmount = 270.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_parent != null)
+        {
+            //Rotates the lever to face the target
+            this.transform.LookAt(m_parent.transform.position);
+            //Adjust the rotation so it correctly faces the players hand and prevents roation on the y and z axis
+            Vector3 rotation = -this.transform.rotation.eulerAngles;
+            rotation.y = 0;
+            rotation.z = 0;
+
+            this.transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+
+    public override bool Grab(GameObject parent)
+    {
+        //Returns false if the object is already attached
+        if (m_parent != null)
+        {
+            return false;
+        }
+
+        m_parent = parent;
+
+        return true;
+    }
+
+    public override void Drop()
+    {
+        m_parent = null;
+
+        Debug.Log("Relesed Lever");
+    }
+}
--- Assets/Scripts/Triggers/Activators/ItemTrigger.cs
+++ Assets/Scripts/Triggers/Activators/ItemTrigger.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ItemTrigger : Trigger
 {
-    public int test;
-
     // Start is called before the first frame update
     void Start()
     {
