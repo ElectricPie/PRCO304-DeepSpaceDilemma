b0a1f0265a5beff3933fd3e7b9cdbdec131b7115
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-27 17:15:30 +0000

    Connected Client To Server
    Using Photon the client connected to the server and attempts to join a room, if none are available then it creates one.

---
+++ Assets/Scripts/Networking/Launcher.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Private Serializable Fields
+    [Tooltip("The maximum number of players a room can hold")]
+    [SerializeField]
+    private byte m_maxPlayersPerRoom = 5;
+    #endregion
+
+
+    #region Private Fields
+    private string gameVersion = "1";
+    #endregion
+
+
+    #region MonoBehaviour CallBacks
+    void Awake()
+    {
+        //Makes sure all clients in the room use the same scene
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+    #endregion
+
+
+    #region MonoBehaviourPunCallbacks Callbacks
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconect() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN PUN Basics Tutorial/Launcher: OnJoinRandomFailed() was called by PUN. No random romms available. Creating room");
+
+        //Creates a new room if no avaible rooms are found
+        PhotonNetwork.CreateRoom(null, new RoomOptions{ MaxPlayers = m_maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN PUN Basics Tutorial/Launcher: OnJoinedRoom() was called by PUN. Now in a room");
+    }
+    #endregion
+
+
+    #region Public Methods
+    public void Connect()
+    {
+        //Checks if the client is connected, if connected it will join a random room else it will attempt to connect to the server
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+    #endregion
+}
