7b12ea1daa99140c3d41d7d7e16ec1aa3f60b469
Connor Priest <Kon_Priest@Hotmail.com>
2020-02-29 00:00:04 +0000

    Added Scene Change After Joning a Room
    Added a button to the laucher scene which allows the player to choose when to connect to the server. When the client connects to the server it will change the scene to the correct scene.

---
+++ Assets/Scripts/Networking/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+namespace Net.ObjectiveComplete.DeepSpaceDilemma
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        #region Photon Callbacks
+        public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", newPlayer.NickName);
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+                LoadScene();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", otherPlayer.NickName);
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+
+                LoadScene();
+            }
+        }
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+        #endregion
+
+        #region Public Methods
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        #endregion
+
+        #region Private Methods
+        private void LoadScene()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork: Trying to load scene but this is not the master client");
+            }
+            Debug.LogFormat("PhotonNetwork: Loading level, Player Count: {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("NetworkTestPlayers" + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+        #endregion
+    }
+}
