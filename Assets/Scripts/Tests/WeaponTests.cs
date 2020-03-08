7ea31bcc4c2667e754cbdb9c922698b7fa369b7a
Connor Priest <Kon_Priest@Hotmail.com>
2020-03-08 13:48:20 +0000

    Create Weapon Test Script
    Created a weapon test script that calls the interact method on the weapon that it is a component of. This allows weapons to be tested without VR.

---
+++ Assets/Scripts/Tests/WeaponTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponTests : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Fire", 1.0f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Private Methods
+    private void Fire()
+    {
+        //Fire the weapon
+        this.GetComponent<Weapon>().Interact();
+    }
+    #endregion
+}
