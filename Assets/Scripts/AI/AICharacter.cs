96ae8d1d7d85070da4a5be1588fab85154edb2c2
Connor Priest <Kon_Priest@Hotmail.com>
2020-03-13 22:48:48 +0000

    Added AI Targeting
    AI character has a sphere trigger that adds any characters that moves into the sphere to a list. If the AI has a line of sight using a raycast the enemy will set the object in sight as its target. The AI will set its destination as the targets position.

--- Assets/Scripts/AI/AICharacter.cs
+++ Assets/Scripts/AI/AICharacter.cs
@@ -7,11 +7,17 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class AICharacter : Character
 {
-    #region Private Vairables
+    #region Private Serialize Vairables
     [Tooltip("The world location the AI will move to")]
     [SerializeField]
     private Vector3 m_destination;
+    #endregion
+
+
+    #region Private Vairables
     private NavMeshAgent m_agent;
+    private List<GameObject> m_charactersInRange;
+    private GameObject m_target;
     #endregion
 
 
@@ -19,17 +25,53 @@ public class AICharacter : Character
     void Awake()
     {
         m_agent = this.GetComponent<NavMeshAgent>();
+        m_charactersInRange = new List<GameObject>();
+        m_target = null;
     }
 
     void Start()
     {
-        MoveToDestination();
+        //MoveToDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Looks for a target if it one is within its trigger sphere
+        if (m_target == null)
+        {
+            foreach (GameObject character in m_charactersInRange)
+            {
+                RaycastHit hit;
+                //Create a raycast from the AI to the character to see if anything is in the way
+                if (Physics.Raycast(this.transform.localPosition, character.transform.position - this.transform.position, out hit, Mathf.Infinity))
+                {
+                    if (hit.transform.GetComponent<Character>())
+                    {
+                        m_target = hit.transform.gameObject;
+                        MoveToDestination();
+                        Debug.Log("Ai has set <a>target</a>", m_target);
+                    }
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Character>())
+        {
+            m_charactersInRange.Add(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        m_charactersInRange.Remove(other.gameObject);
+        if (other.gameObject == m_target)
+        {
+            m_target = null;
+        }
     }
     #endregion
 
@@ -37,7 +79,7 @@ public class AICharacter : Character
     #region Private Methods
     private void MoveToDestination()
     {
-        m_agent.SetDestination(m_destination);
+        m_agent.SetDestination(m_target.transform.position);
     }
     #endregion
 
