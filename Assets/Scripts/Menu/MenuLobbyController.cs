6e8941b52962a46fe14b3d770a962ba2f5430072
Connor Priest <Kon_Priest@Hotmail.com>
2020-03-25 15:14:07 +0000

    Created Basic Word Input
    Create a trigger that the buttons activate which sends a character to the menu lobby controller. The controller adds the character to the code string and displays it on the console.

---
+++ Assets/Scripts/Menu/MenuLobbyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MenuLobbyController : MonoBehaviour
+{
+    #region Private Variables
+    [Tooltip("The text mesh object that will display the code")]
+    [SerializeField]
+    private TextMesh m_codeDisplay;
+
+    private string m_enteredCode;
+    #endregion
+
+
+    #region Monobehaviour Callbacks
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_enteredCode = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+
+    #region Private Methods
+    private void UpdateText()
+    {
+        m_codeDisplay.text = m_enteredCode;
+    }
+    #endregion
+
+
+    #region Public Methods
+    public void EnterCodeCharacter(char codeValue)
+    {
+        m_enteredCode += codeValue;
+        Debug.Log("Entered: " + m_enteredCode);
+        UpdateText();
+    }
+    #endregion
+}
