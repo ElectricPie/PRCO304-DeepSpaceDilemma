3c1ae16515a847c4c40768171e2eaa13f9332c3d
Connor Priest <Kon_Priest@Hotmail.com>
2020-04-23 23:51:17 +0100

    Implemented Ammo Pouch
    Created a cube attached to the player body which when grabbed by the player will create a magazine specified by the weapon and the magazine will be grabbed by the players hand.

--- Assets/Scripts/VR/Grab.cs
+++ Assets/Scripts/VR/Grab.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 using UnityEngine.XR;
 
 public class Grab : MonoBehaviour
-{    
-    //Public
-    public bool isLeftHand;
+{
+    #region Private Serialized Variables
+    [Tooltip("Sets if the controller acts a the left hand")]
+    [SerializeField]
+    private bool m_isLeftHand = false;
+    [Tooltip("The game object with the ammo pouch script")]
+    [SerializeField]
+    private AmmoPouch m_ammoPouch = null;
+    #endregion
 
-    //Private 
-    private string m_handGrip;
-    private GameObject m_lastCollision;
-    private GameObject m_grabbedObject;
 
+    #region Private Variables 
+    private string m_handGrip = "";
+    private GameObject m_lastCollision = null;
+    private GameObject m_grabbedObject = null;
+    #endregion
+
+
+    #region MonoBehaviour Callbacks
     // Start is called before the first frame update
     void Start()
     {
-        if (isLeftHand)
+        if (m_isLeftHand)
         {
             m_handGrip = "Left";
         }
@@ -37,10 +47,25 @@ public class Grab : MonoBehaviour
             {
                 if (m_lastCollision != null)
                 {
-                    //Attempt to grab the object
-                    if (m_lastCollision.GetComponent<GrabableObject>().Grab(this.gameObject)) {
+                    GameObject magazine = null;
+
+                    //Attempts to access a ammo pouch and if that fails attempt to grab the object
+                    if (m_lastCollision.GetComponent<AmmoPouch>())
+                    {
+                        m_lastCollision.GetComponent<AmmoPouch>().Grab(this.gameObject, out magazine);
+                        magazine.GetComponent<GrabableObject>().Grab(this.gameObject);
+                        m_grabbedObject = magazine;
+                        m_lastCollision = null;
+                    }
+                    else if (m_lastCollision.GetComponent<GrabableObject>().Grab(this.gameObject))
+                    {
                         m_grabbedObject = m_lastCollision;
                         m_lastCollision = null;
+
+                        if (m_grabbedObject.GetComponent<Weapon>())
+                        {
+                            m_ammoPouch.MagazineType = m_grabbedObject.GetComponent<Weapon>().MagazineType;
+                        }
                     }
                 }
             }
@@ -61,18 +86,6 @@ public class Grab : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Forces the character to drop a object
-    /// </summary>
-    public void DropObject()
-    {
-        if (m_grabbedObject != null)
-        {
-            m_grabbedObject.GetComponent<GrabableObject>().Drop();
-            m_grabbedObject = null;
-        }
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         //Checks of the object has the grabable script
@@ -90,9 +103,35 @@ public class Grab : MonoBehaviour
             m_lastCollision = null;
         }
     }
+    #endregion
 
+
+    #region Public Methods
+    /// <summary>
+    /// Forces the character to drop a object
+    /// </summary>
+    public void DropObject()
+    {
+        if (m_grabbedObject != null)
+        {
+            m_grabbedObject.GetComponent<GrabableObject>().Drop();
+
+            //Resets the ammo pouch
+            if (m_grabbedObject.GetComponent<Weapon>())
+            {
+                m_ammoPouch.MagazineType = null;
+            }
+
+            m_grabbedObject = null;
+        }
+    }
+    #endregion
+
+
+    #region Properties
     public string Hand
     {
         get { return m_handGrip; }
     }
+    #endregion
 }
---
+++ Assets/Scripts/Weapons/AmmoPouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPouch : GrabableObject
+{
+    #region Private Variables
+    private GameObject m_magazineType = null;
+    #endregion
+
+    public bool Grab(GameObject parent, out GameObject magazine)
+    {
+        //If a magazine type is set, create that magazine and set it to the parents position
+        if (m_magazineType != null) {
+            magazine = Instantiate(m_magazineType);
+            magazine.transform.position = parent.transform.position;
+            return true;
+        }
+
+        magazine = null;
+        return false;
+    }
+
+    #region Properties
+    public GameObject MagazineType
+    {
+        set { m_magazineType = value; }
+    }
+    #endregion
+}
