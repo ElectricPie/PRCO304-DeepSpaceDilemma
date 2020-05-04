60b580902d8c3cf76946885e82caeed969471792
Connor Priest <Kon_Priest@Hotmail.com>
2020-05-04 12:16:28 +0100

    Created Secondary Grab Point for Weapons
    Created a new object which is a child of the weapon.
    When the secondary grab point is grabbed by a hand the hand will be set as a child of the grab point.

---
+++ Assets/Scripts/Weapons/SecondaryGrabPoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondaryGrabPoint : GrabableObject
+{
+    #region Private Variables
+ 
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Public Methods
+    public override bool Grab(GameObject parent)
+    {
+        GameObject handChild = parent.transform.GetChild(0).gameObject;
+        //return base.Grab(parent);
+        handChild.transform.parent = this.gameObject.transform;
+        handChild.transform.position = this.transform.position;
+
+        return true;
+    }
+
+    public override void Drop()
+    {
+        //base.Drop();
+    }
+    #endregion
+
+
+    #region Gizmo Methods
+    void OnDrawGizmos()
+    {
+        //Create a sphere where the grab point will be
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(this.transform.position, 0.05f);
+    }
+    #endregion
+}
