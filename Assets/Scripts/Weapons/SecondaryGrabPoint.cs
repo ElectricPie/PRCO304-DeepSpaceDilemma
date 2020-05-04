5d4229e1d4fafa7a995313c75400c5da4fa8a2ff
Connor Priest <Kon_Priest@Hotmail.com>
2020-05-04 15:11:26 +0100

    Hand Now Attaches Properly to Secondary Grip
    The hand now is set as a child to the secondary grip point and the hand correctly has it position and rotation set.

--- Assets/Scripts/Weapons/SecondaryGrabPoint.cs
+++ Assets/Scripts/Weapons/SecondaryGrabPoint.cs
@@ -5,35 +5,32 @@ using UnityEngine;
 public class SecondaryGrabPoint : GrabableObject
 {
     #region Private Variables
- 
+    private GameObject m_hand = null;
     #endregion
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     #region Public Methods
     public override bool Grab(GameObject parent)
     {
-        GameObject handChild = parent.transform.GetChild(0).gameObject;
-        //return base.Grab(parent);
-        handChild.transform.parent = this.gameObject.transform;
-        handChild.transform.position = this.transform.position;
-
+        m_parent = parent;
+        m_hand = parent.transform.GetChild(0).gameObject;
+        
+        m_hand.transform.parent = this.gameObject.transform;
+        m_hand.transform.position = this.transform.position;
+        m_hand.transform.localRotation = Quaternion.Euler( new Vector3(3.0f, 125.0f, 270.0f));
         return true;
     }
 
     public override void Drop()
     {
-        //base.Drop();
+        //Resets the hand to the parents postion
+        m_hand.transform.parent = m_parent.transform;
+        m_hand.transform.position = m_parent.transform.position;
+        m_hand.transform.rotation = m_parent.transform.rotation;
+
+        //Removes the refences to the hand and parent
+        m_parent = null;
+        m_hand = null;
     }
     #endregion
 
